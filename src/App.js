b4f780d5f5b046bb5d6a8db78b58a9ae7db3cb7f
gzj <1131600386@qq.com>
2024-04-30 01:25:25 +0800

    sixth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState,useCallback } from 'react';
+import { createEditor } from 'slate';
+import { Slate, Editable, withReact } from 'slate-react';
 
-function App() {
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }],
+  },
+];
+
+const App = () => {
+  const editor = useMemo(() => withReact(createEditor()), []);
+
+  const [isBlockquote, setIsBlockquote] = useState(false);
+
+  const renderElement = useCallback((props) => {
+    return isBlockquote ? <BlockquoteElement {...props} /> : <ParagraphElement {...props} />;
+  }, [isBlockquote]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Slate editor={editor} initialValue={initialValue}>
+      <div>
+        <button onClick={() => setIsBlockquote(!isBlockquote)}>
+          {isBlockquote ? '切换为段落' : '切换为引用'}
+        </button>
+      </div>
+      <Editable renderElement={renderElement} />
+    </Slate>
   );
-}
+};
+
+const BlockquoteElement = ({ attributes, children }) => (
+  <blockquote {...attributes}>{children}</blockquote>
+);
+
+const ParagraphElement = ({ attributes, children }) => (
+  <p {...attributes}>{children}</p>
+);
 
 export default App;
