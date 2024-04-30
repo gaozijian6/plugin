469421d3340b95000937fe2098dd135e063cdeeb
高子健 <gaozijian@gaozijiandeMacBook-Pro.local>
2024-04-30 15:06:47 +0800

    eight

--- src/App.js
+++ src/App.js
@@ -1,41 +1,126 @@
-import React, { useMemo, useState,useCallback } from 'react';
-import { createEditor } from 'slate';
-import { Slate, Editable, withReact } from 'slate-react';
+import React, { useCallback, useMemo } from 'react'
+import { Editable, withReact, useSlate, Slate } from 'slate-react'
+import {
+  Editor,
+  Transforms,
+  createEditor,
+  Element as SlateElement,
+} from 'slate'
+import { withHistory } from 'slate-history'
+import './App.css'
 
-const initialValue = [
-  {
-    type: 'paragraph',
-    children: [{ text: 'A line of text in a paragraph.' }],
-  },
-];
+
+const LIST_TYPES = ['numbered-list', 'bulleted-list']
+const TEXT_ALIGN_TYPES = ['left', 'center', 'right', 'justify']
 
 const App = () => {
-  const editor = useMemo(() => withReact(createEditor()), []);
-
-  const [isBlockquote, setIsBlockquote] = useState(false);
-
-  const renderElement = useCallback((props) => {
-    return isBlockquote ? <BlockquoteElement {...props} /> : <ParagraphElement {...props} />;
-  }, [isBlockquote]);
+  const renderElement = useCallback(props => <Element {...props} />, [])
+  const editor = useMemo(() => withHistory(withReact(createEditor())), [])
 
   return (
     <Slate editor={editor} initialValue={initialValue}>
-      <div>
-        <button onClick={() => setIsBlockquote(!isBlockquote)}>
-          {isBlockquote ? '切换为段落' : '切换为引用'}
-        </button>
-      </div>
+        <BlockButton format="block-quote" />
       <Editable renderElement={renderElement} />
     </Slate>
-  );
-};
+  )
+}
 
-const BlockquoteElement = ({ attributes, children }) => (
-  <blockquote {...attributes}>{children}</blockquote>
-);
+const toggleBlock = (editor, format) => {
+  const isActive = isBlockActive(
+    editor,
+    format,
+    TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type'
+  )
+  const isList = LIST_TYPES.includes(format)
 
-const ParagraphElement = ({ attributes, children }) => (
-  <p {...attributes}>{children}</p>
-);
+  Transforms.unwrapNodes(editor, {
+    match: n =>
+      !Editor.isEditor(n) &&
+      SlateElement.isElement(n) &&
+      LIST_TYPES.includes(n.type) &&
+      !TEXT_ALIGN_TYPES.includes(format),
+    split: true,
+  })
+  let newProperties
+  if (TEXT_ALIGN_TYPES.includes(format)) {
+    newProperties = {
+      align: isActive ? undefined : format,
+    }
+  } else {
+    newProperties = {
+      type: isActive ? 'paragraph' : isList ? 'list-item' : format,
+    }
+  }
+  Transforms.setNodes(editor, newProperties)
 
-export default App;
+  if (!isActive && isList) {
+    const block = { type: format, children: [] }
+    Transforms.wrapNodes(editor, block)
+  }
+}
+
+const isBlockActive = (editor, format, blockType = 'type') => {
+  const { selection } = editor
+  if (!selection) return false
+  const [match] = Array.from(
+    Editor.nodes(editor, {
+      at: Editor.unhangRange(editor, selection),
+      match: n =>
+        !Editor.isEditor(n) &&
+        SlateElement.isElement(n) &&
+        n[blockType] === format,
+    })
+  )
+  return !!match
+}
+
+const Element = ({ attributes, children, element }) => {
+  const style = { textAlign: element.align }
+  switch (element.type) {
+    case 'block-quote':
+      return (
+        <blockquote style={style} {...attributes}>
+          {children}
+        </blockquote>
+      )
+    default:
+      return (
+        <p style={style} {...attributes}>
+          {children}
+        </p>
+      )
+  }
+}
+
+const BlockButton = ({ format}) => {
+  const editor = useSlate()
+  return (
+    <button
+      className={isBlockActive(
+        editor,
+        format,
+        TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type'
+      )? 'active' : ''}
+      onMouseDown={event => {
+        event.preventDefault()
+        toggleBlock(editor, format)
+      }}
+    >
+      {format}
+    </button>
+  )
+}
+
+const initialValue = [
+  {
+    type: 'block-quote',
+    children: [{ text: 'A wise quote.' }],
+  },
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text' }],
+  },
+]
+
+export default App
+
